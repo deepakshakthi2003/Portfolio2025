8cc8b11bc9881beb884661adff349a734934b086
V. Deepak <deepakshakthi2003@gmail.com>
2025-05-14 19:24:57 +0530

    Fix base path in vite.config.js for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio2025/',
+  base: './',
   plugins: [react()],
   server: {
     open: false, // ✅ Prevents browser from opening
